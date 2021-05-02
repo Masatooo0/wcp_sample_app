a94bb9ffc21139fe6d7f7088794128f16c517206
“Masatooo0” <masato.n.0321@gmail.com>
2021-05-02 13:10:39 +0000

    [add]画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b32fe868cd26be2188a7ace73ed9d24c36522a0c39e5166389378ae8ee52c8a559b6eecc68026def53d16139cb3660852218661877e563c32348be70b9ef58b4'
