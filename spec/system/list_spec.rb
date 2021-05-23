1ab4eb844eb28fa45a8fb228bb1d5828e69b9a83
Masatooo0 <masato.n.0321@gmail.com>
2021-05-23 08:34:35 +0000

    [add]テスト

--- spec/system/list_spec.rb
+++ spec/system/list_spec.rb
@@ -14,5 +14,31 @@ describe '投稿のテスト' do
         expect(current_path).to eq('/top')
       end
     end
-  end 
+  end
+  describe '投稿画面のテスト' do
+    before do
+      visit todolists_new_path
+    end
+    context '表示の確認' do
+      it 'todolists_new_pathが"/todolists/new"であるか' do
+        expect(current_path).to eq ('/todolists/new')
+      end
+      it '投稿ボタンが表示されているか' do
+        expect(page).to have_button '投稿'
+      end
+    end
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        fill_in 'list[title]',with:Faker::Lorem.characters(number:5)
+        fill_in 'list[body]',with:Faker::Lorem.characters(number:20)
+        click_button '投稿'
+        expect(page).to have_current_path todolist_path(List.last)
+      end
+    end
+  end
+  describe '一覧画面のテスト' do
+    before do
+      visit todolists_path
+    end
+  end
 end
